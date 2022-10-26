92586d84027c275c2b54d0c23e3b511d26e3c728
Morsalin3 <mdmorsalin3@gmail.com>
2022-10-26 10:59:20 +0600

    create api

--- index.js
+++ index.js
@@ -5,11 +5,26 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./data/categories.json');
 const courses = require('./data/courses.json');
 
 app.get('/',(req, res)=>{
     res.send('learn programming api running');
 });
+
+app.get('/categories', (req, res) =>{
+    res.send(categories)
+})
+
+app.get('/courses', (req, res) =>{
+    res.send(courses)
+})
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectCourse = courses.filter(course => course.category_id == id);
+    res.send(selectCourse);
+})
 
 app.listen(port, () =>{
     console.log('learn progarmming server running on port', port);
